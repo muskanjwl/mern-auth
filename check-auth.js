82a703a48bab608f3e5ace0e2adc9c47cadce01f
muskanjwl <muskanjwl@gmail.com>
2021-07-10 16:56:01 +0530

    typo

--- check-auth.js
+++ check-auth.js
@@ -1,10 +1,14 @@
 const jwt = require("jsonwebtoken");
-const key = require("./config/keys.js").secretorkey;
+const key = require("./config/keys.js").secretOrKey;
 module.exports = (req, res, next) => {
     try{
         const decoded =jwt.verify(req.body.token,key);
+        req.User =decoded;
+       console.log('kk');
         next();
     }catch(error){
+        console.log('hi');
+       console.log(req.json);
         return res.status(400).json({
             message:'login first'
         });
